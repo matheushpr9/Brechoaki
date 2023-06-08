7b7b3135c49df3b756b86deda7ff17a6dba16444
MatheusPtasinski <matheus.ptasinski@dafiti.com.br>
2023-06-08 20:43:42 -0300

    created and referenced the templates folder, configured the file gallery/urls.py to render the HTML files from the templates folder

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<H1>Brechoaki</H1><p>Bem vindo!</p>')
+    return render(request, 'index.html')
