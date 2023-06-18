3d62fd4639b9e9c50cceeb6e75bc28fae061bd37
MatheusPtasinski <matheus.ptasinski@dafiti.com.br>
2023-06-18 10:27:43 -0300

    created the login and the cadastro url

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from usuarios.views import login, cadastro
+# path(urlNoSite, MetodoChamado,nome )
 
 urlpatterns = [
-    
+    path('login', login, name="login"),
+    path('cadastro', cadastro, name="cadastro"),
 ]
