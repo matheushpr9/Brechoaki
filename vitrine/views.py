49d84e634348eb54d56bcc3582aa4788d1a51aed
MatheusPtasinski <matheus.ptasinski@dafiti.com.br>
2023-06-18 15:52:07 -0300

    add products

--- vitrine/views.py
+++ vitrine/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from vitrine.models import Produto
 
 
@@ -6,7 +6,9 @@ def index(request):
     data = Produto.objects.order_by("criadoEm").filter(disponibilidade =True)
     return render(request, 'vitrine/index.html', {'cards':data})
 
+
 def imagem(request, item_id):
+    
     item = get_object_or_404(Produto, pk=item_id)
     return render(request, 'vitrine/imagem.html', {"item":item})
 
